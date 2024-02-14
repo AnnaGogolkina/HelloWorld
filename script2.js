7aaf18d9cae1ae4652ac65d79cdc3f656047c1f2
Anna Gogolkina <a-chuikova@mail.ru>
2024-02-14 20:07:54 +0300

    add hw-3

---
+++ script2.js
@@ -0,0 +1,60 @@
+let password = "пароль"
+let yourPassword("Введите пароль");
+
+if (yourPassword===password){
+    alert ("Пароль введен верно");
+}
+
+else{
+    alert("Пароль введен неверно");
+}
+
+let c = 0
+if ( c > 0 && c < !10); 
+console.log('Верно');
+else 
+console.log ('Неверно');
+
+let d = 10
+let e = 200
+
+if (d > 100 || e > 100){
+    console.log ("Верно");
+}
+
+else
+console.log {
+    console.log ("Неверно");
+}
+
+let a = "2"
+let b = "3"
+alert (Number(a) + Number(b));
+
+
+let monthNumber = 12
+
+switch (month) {
+    case 1:
+    case 2:
+    case 12:
+    alert ("Зима");
+    break;
+    case 3:
+    case 4:
+    case 5:
+    alert ("Весна");
+    break;
+    case 6:
+    case 7:
+    case 8:
+    alert ("Лето");
+    break;
+    case 9:
+    case 10:
+    case 11:
+    alert ("Осень");
+    break;
+    default: 
+    alert ( "Неизвестный меясц");
+}
